c39d977f9af984b551d96d703d2fbdd36a3ba705
moadhamousti <moadhamousti@gmail.com>
2024-09-30 16:38:28 +0100

    tRPC Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import Navbar from '@/components/Navbar'
-// import Providers from '@/components/Providers'
-import { cn } from '@/lib/utils'
+import Providers from '@/components/Providers'
+import { 
+  cn, 
+  // constructMetadata
+} from '@/lib/utils'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
 // import 'react-loading-skeleton/dist/skeleton.css'
 // import 'simplebar-react/dist/simplebar.min.css'
 
+// import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,16 +23,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className='light'>
-      {/* <Providers> */}
+      <Providers>
         <body
           className={cn(
             'min-h-screen font-sans antialiased grainy',
             inter.className
           )}>
+          {/* <Toaster /> */}
           <Navbar />
           {children}
         </body>
-      {/* </Providers> */}
+      </Providers>
     </html>
   )
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,7 @@
 import { TRPCError } from '@trpc/server';
 import { publicProcedure, router } from './trpc';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { db } from '@/db';
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -14,6 +15,22 @@ export const appRouter = router({
 
     if (!user || !user.id || !user.email) {
       throw new TRPCError({ code: 'UNAUTHORIZED'});
+    }
+
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    })
+
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      })
     }
 
     return { success: true };
