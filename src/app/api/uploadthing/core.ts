dd1e0758986f98a8493cc46174d7c2a41b8c4770
moadhamousti <moadhamousti@gmail.com>
2024-10-03 19:13:24 +0100

    Update File Page

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -25,6 +25,7 @@ export const ourFileRouter = {
           name: file.name,
           userId: metadata.userId,
           url: `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`,
+          // url: file.url,
           uploadStatus: 'PROCESSING',
         },
       });
--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,8 +1,8 @@
-import PdfRenderer from "@/components/PdfRenderer"
 import { db } from "@/db"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { redirect } from "next/navigation"
+import { notFound, redirect } from "next/navigation"
 import ChatWrapper from '@/components/chat/ChatWrapper'
+import PdfRenderer from "@/components/PdfRenderer"
 
 interface PageProps {
     params: {
@@ -12,20 +12,22 @@ interface PageProps {
 
 
   const Page = async ({ params }: PageProps) => {
-    const {fileid} = params
-
+    const { fileid } = params
+  
     const { getUser } = getKindeServerSession()
     const user = getUser()
-
+  
     if (!user || !(await user).id)
-        redirect(`/auth-callback?origin=dashboard/${fileid}`)
-
+      redirect(`/auth-callback?origin=dashboard/${fileid}`)
+  
     const file = await db.file.findFirst({
-        where: {
+      where: {
         id: fileid,
         userId: (await user).id,
-        },
-    }) 
+      },
+    })
+  
+    if (!file) notFound()
 
 
     return (
@@ -33,7 +35,7 @@ interface PageProps {
             <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
                 <div className='flex-1 xl:flex'>
                     <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
-                        <PdfRenderer />
+                        <PdfRenderer url={file.url}/>
                     </div>
                 </div>
 
