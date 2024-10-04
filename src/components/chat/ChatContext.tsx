3d4c368bd368ad090790aabf96fe73a1aa732277
moadhamousti <moadhamousti@gmail.com>
2024-10-04 21:57:46 +0100

    Update Chat

---
+++ src/components/chat/ChatContext.tsx
@@ -0,0 +1,24 @@
+import { createContext } from 'react'
+
+type StreamResponse = {
+    addMessage: () => void
+    message: string
+    handleInputChange: (
+      event: React.ChangeEvent<HTMLTextAreaElement>
+    ) => void
+    isLoading: boolean
+  }
+  
+
+  export const ChatContext = createContext<StreamResponse>({
+    addMessage: () => {},
+    message: '',
+    handleInputChange: () => {},
+    isLoading: false,
+  })
+  
+
+
+//   export const ChatContextProvider = ({fileId,children}: Props) => {
+    
+//   }
