9bccf11227a874d0e8ef4f64daaaf802da7a4f21
moadhamousti <moadhamousti@gmail.com>
2024-10-04 17:42:04 +0100

    Update File Page

--- src/components/PdfRenderer.tsx
+++ src/components/PdfRenderer.tsx
@@ -1,36 +1,57 @@
 'use client'
 
-
-import { ChevronDown, ChevronUp, Loader2, Search } from 'lucide-react';
+import {
+  ChevronDown,
+  ChevronUp,
+  Loader2,
+  RotateCw,
+  Search,
+} from 'lucide-react'
 import { Document, Page, pdfjs } from 'react-pdf'
-import { useResizeDetector } from 'react-resize-detector'
+
 import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
-import { useToast } from "@/hooks/use-toast"
-import { Button } from './ui/button';
-import { Input } from './ui/input';
-import { useState } from 'react';
+// import { useToast } from './ui/use-toast'
+
+import { useResizeDetector } from 'react-resize-detector'
+import { Button } from './ui/button'
+import { Input } from './ui/input'
+import { useState } from 'react'
+
 import { useForm } from 'react-hook-form'
-import { z } from 'zod';
+import { z } from 'zod'
+
 import { zodResolver } from '@hookform/resolvers/zod'
-import { cn } from '@/lib/utils';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { cn } from '@/lib/utils'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
+
 import SimpleBar from 'simplebar-react'
-pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.mjs";
+import PdfFullscreen from './PdfFullscreen'
+import { useToast } from '@/hooks/use-toast'
+
+pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.mjs"
 
 interface PdfRendererProps {
   url: string
 }
 
-
 const PdfRenderer = ({ url }: PdfRendererProps) => {
-
   const { toast } = useToast()
 
   const [numPages, setNumPages] = useState<number>()
   const [currPage, setCurrPage] = useState<number>(1)
   const [scale, setScale] = useState<number>(1)
   const [rotation, setRotation] = useState<number>(0)
+  const [renderedScale, setRenderedScale] = useState<
+    number | null
+  >(null)
+
+  const isLoading = renderedScale !== scale
 
   const CustomPageValidator = z.object({
     page: z
@@ -44,7 +65,6 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
     typeof CustomPageValidator
   >
 
-
   const {
     register,
     handleSubmit,
@@ -57,6 +77,8 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
     resolver: zodResolver(CustomPageValidator),
   })
 
+  console.log(errors)
+
   const { width, ref } = useResizeDetector()
 
   const handlePageSubmit = ({
@@ -65,7 +87,6 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
     setCurrPage(Number(page))
     setValue('page', String(page))
   }
-
 
   return (
     <div className='w-full bg-white rounded-md shadow flex flex-col items-center'>
@@ -77,7 +98,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               setCurrPage((prev) =>
                 prev - 1 > 1 ? prev - 1 : 1
               )
-              // setValue('page', String(currPage - 1))
+              setValue('page', String(currPage - 1))
             }}
             variant='ghost'
             aria-label='previous page'>
@@ -112,7 +133,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               setCurrPage((prev) =>
                 prev + 1 > numPages! ? numPages! : prev + 1
               )
-              // setValue('page', String(currPage + 1))
+              setValue('page', String(currPage + 1))
             }}
             variant='ghost'
             aria-label='next page'>
@@ -123,7 +144,7 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
         <div className='space-x-2'>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button 
+              <Button
                 className='gap-1.5'
                 aria-label='zoom'
                 variant='ghost'>
@@ -151,41 +172,70 @@ const PdfRenderer = ({ url }: PdfRendererProps) => {
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
+
+          <Button
+            onClick={() => setRotation((prev) => prev + 90)}
+            variant='ghost'
+            aria-label='rotate 90 degrees'>
+            <RotateCw className='h-4 w-4' />
+          </Button>
+
+          <PdfFullscreen fileUrl={url} />
         </div>
       </div>
 
-      <div className="flex-1 w-full max-h-screen">
-        <SimpleBar autoHide={false} className='max-h-[calc(100vh-10rem)]'/>
-        {/* <SimpleBar autoHide={false} className='max-h-[calc(100vh-10rem)]'> */}
-        <div ref={ref}>
-          <Document 
-            loading={
-              <div className='flex justify-center'>
-                <Loader2 className='my-24 h-6 w-6 animate-spin' />
-              </div>
-            }
-            onLoadError={() => {
-              toast({
-                title: 'Error loading PDF',
-                description: 'Please try again later',
-                variant: 'destructive',
-              })
-            }}
-            onLoadSuccess={({ numPages }) =>
-              setNumPages(numPages)
-            }
-            file={url} 
-            className='max-h-full'>
-          <Page
-            width={width ? width : 1}
-            pageNumber={currPage}
-            scale={scale}
-            // rotate={rotation}
-            // key={'@' + renderedScale}
-          />
-          
-          </Document>
-        </div>
+      <div className='flex-1 w-full max-h-screen'>
+        <SimpleBar
+          autoHide={false}
+          className='max-h-[calc(100vh-10rem)]'>
+          <div ref={ref}>
+            <Document
+              loading={
+                <div className='flex justify-center'>
+                  <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                </div>
+              }
+              onLoadError={() => {
+                toast({
+                  title: 'Error loading PDF',
+                  description: 'Please try again later',
+                  variant: 'destructive',
+                })
+              }}
+              onLoadSuccess={({ numPages }) =>
+                setNumPages(numPages)
+              }
+              file={url}
+              className='max-h-full'>
+              {isLoading && renderedScale ? (
+                <Page
+                  width={width ? width : 1}
+                  pageNumber={currPage}
+                  scale={scale}
+                  rotate={rotation}
+                  key={'@' + renderedScale}
+                />
+              ) : null}
+
+              <Page
+                className={cn(isLoading ? 'hidden' : '')}
+                width={width ? width : 1}
+                pageNumber={currPage}
+                scale={scale}
+                rotate={rotation}
+                key={'@' + scale}
+                loading={
+                  <div className='flex justify-center'>
+                    <Loader2 className='my-24 h-6 w-6 animate-spin' />
+                  </div>
+                }
+                onRenderSuccess={() =>
+                  setRenderedScale(scale)
+                }
+              />
+            </Document>
+          </div>
+        </SimpleBar>
       </div>
     </div>
   )
--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,5 +1,33 @@
-const ChatWrapper = () => {
-    return <div>ChatWrapper</div>
+import { trpc } from "@/app/_trpc/client";
+import ChatInput from "./ChatInput"
+import Messages from "./Messages"
+
+
+interface ChatWrapperProps {
+    fileId: string
+}
+
+
+const ChatWrapper = ({fileId}: ChatWrapperProps) => {
+    const { data, isLoading } = trpc.getFileUploadStatus.useQuery({
+        fileId,
+    },
+        {
+            refetchInterval: (data) => {
+              return data?.status === 'SUCCESS' || data?.status === 'FAILED' ? false : 500;
+            },
+          }
+    )
+
+      
+    return (
+        <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
+            <div className='flex-1 justify-between flex flex-col mb-28'>
+                <Messages />
+            </div>
+            <ChatInput />
+        </div>
+    )
 }
 
 export default ChatWrapper
