5342e52088e05784c87d74b9f7821d0e157e6fce
moadhamousti <moadhamousti@gmail.com>
2024-11-27 11:33:57 +0100

    Update

--- src/components/chat/ChatContext.tsx
+++ src/components/chat/ChatContext.tsx
@@ -1,79 +1,79 @@
-import { trpc } from '@/app/_trpc/client'
-import { useToast } from '@/hooks/use-toast'
-import { useMutation } from '@tanstack/react-query'
-import { createContext, ReactNode, useRef, useState } from 'react'
+// import { trpc } from '@/app/_trpc/client'
+// import { useToast } from '@/hooks/use-toast'
+// import { useMutation } from '@tanstack/react-query'
+// import { createContext, ReactNode, useRef, useState } from 'react'
 
-type StreamResponse = {
-    addMessage: () => void
-    message: string
-    handleInputChange: (
-      event: React.ChangeEvent<HTMLTextAreaElement>
-    ) => void
-    isLoading: boolean
-  }
+// type StreamResponse = {
+//     addMessage: () => void
+//     message: string
+//     handleInputChange: (
+//       event: React.ChangeEvent<HTMLTextAreaElement>
+//     ) => void
+//     isLoading: boolean
+//   }
   
 
-  export const ChatContext = createContext<StreamResponse>({
-    addMessage: () => {},
-    message: '',
-    handleInputChange: () => {},
-    isLoading: false,
-  })
+//   export const ChatContext = createContext<StreamResponse>({
+//     addMessage: () => {},
+//     message: '',
+//     handleInputChange: () => {},
+//     isLoading: false,
+//   })
 
-  interface Props {
-    fileId: string
-    children: ReactNode
-  }
+//   interface Props {
+//     fileId: string
+//     children: ReactNode
+//   }
   
 
 
-  export const ChatContextProvider = ({fileId,children}: Props) => {
+//   export const ChatContextProvider = ({fileId,children}: Props) => {
 
-    const [message, setMessage] = useState<string>('')
-    const [isLoading, setIsLoading] = useState<boolean>(false)
+//     const [message, setMessage] = useState<string>('')
+//     const [isLoading, setIsLoading] = useState<boolean>(false)
 
-    const utils = trpc.useUtils()
+//     const utils = trpc.useUtils()
 
-    const { toast } = useToast()
+//     const { toast } = useToast()
 
-    const backupMessage = useRef('')
+//     const backupMessage = useRef('')
 
-    const { mutate: sendMessage } = useMutation({
-      mutationFn: async({message} : {message: string}) => {
-        const response = await fetch('/api/message', {
-          method: 'POST',
-          body: JSON.stringify({
-            fileId,
-            message,
-          }),
-        })
-        if (!response.ok) {
-          throw new Error('Failed to send message')
-        }
+//     const { mutate: sendMessage } = useMutation({
+//       mutationFn: async({message} : {message: string}) => {
+//         const response = await fetch('/api/message', {
+//           method: 'POST',
+//           body: JSON.stringify({
+//             fileId,
+//             message,
+//           }),
+//         })
+//         if (!response.ok) {
+//           throw new Error('Failed to send message')
+//         }
 
-        return response.body
-      }
-    })
+//         return response.body
+//       }
+//     })
 
-    const handleInputChange = (
-      e: React.ChangeEvent<HTMLTextAreaElement>
-    ) => {
-      setMessage(e.target.value)
-    }
-
-
-    const addMessage = () => sendMessage({ message })
+//     const handleInputChange = (
+//       e: React.ChangeEvent<HTMLTextAreaElement>
+//     ) => {
+//       setMessage(e.target.value)
+//     }
 
 
-    return (
-      <ChatContext.Provider
-        value={{
-          addMessage,
-          message,
-          handleInputChange,
-          isLoading,
-        }}>
-        {children}
-      </ChatContext.Provider>
-    )
-  }
+//     const addMessage = () => sendMessage({ message })
+
+
+//     return (
+//       <ChatContext.Provider
+//         value={{
+//           addMessage,
+//           message,
+//           handleInputChange,
+//           isLoading,
+//         }}>
+//         {children}
+//       </ChatContext.Provider>
+//     )
+//   }
